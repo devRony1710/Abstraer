c6f93f88be4ee6384162a29136a82becd0136867
Rony Antolinez <rony1710.dev@gmail.com>
2021-01-20 02:39:13 -0500

    Main description complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Form } from './components/Form/Form';
+import { Description } from './components/Description/Description';
+// import { Form } from './components/Form/Form';
 import { Header } from './components/Header/Header';
 import MainSlider from './components/Slider/Slider';
 import './components/styles/Fonts.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <MainSlider />
-      <Form />
-    </div>
+      {/* <Form /> */}
+      <Description />
+    </>
   );
 }
 
