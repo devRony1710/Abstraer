6807af62dd89e7fd7884a6c7a7cd480e89aa246c
Rony Antolinez <rony1710.dev@gmail.com>
2021-01-19 19:12:22 -0500

    building slider component and data of images

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Slider = () => {
+  return (
+    <section>
+      <div></div>
+    </section>
+  );
+};
---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+  {
+    image:
+      'https://res.cloudinary.com/dgthnv9ab/image/upload/v1611101134/slide-3_u2uhdm.jpg',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dgthnv9ab/image/upload/v1611101134/slide-1_kjke3z.jpg',
+  },
+  {
+    image:
+      'https://res.cloudinary.com/dgthnv9ab/image/upload/v1611101135/slide-2_iwime9.jpg',
+  },
+];
